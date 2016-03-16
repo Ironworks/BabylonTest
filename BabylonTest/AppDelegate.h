165cad9234e6b17d03ae358dcf19c7e85f1cea62
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2016-03-16 08:04:32 +0000

    Initial Commit

---
+++ BabylonTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BabylonTest
+//
+//  Created by Trevor Doodes on 16/03/2016.
+//  Copyright © 2016 Ironworks Media Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BabylonTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BabylonTest
+//
+//  Created by Trevor Doodes on 16/03/2016.
+//  Copyright © 2016 Ironworks Media Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
